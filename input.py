910d7feb9c269162addfa593057f44168f5fc83a
vishalg@zignuts.com-at-162555042902 <vishalg@zignuts.com>
2026-03-11 17:53:12 +0530

    input

---
+++ input.py
@@ -0,0 +1,13 @@
+# INPUT
+# In Python, the input() function is used for input.
+# The input() function can be used in two ways.
+# 1.Input without message
+# 2.Input with message
+
+# variable_name = input()
+
+ZIYAN=input("")
+print(ZIYAN)
+
+
+
